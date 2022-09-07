3348e194fc746df687b2b96c0e74ff4b96068d70
OksanaKuzich <komoks255@gmail.com>
2022-09-07 21:45:30 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('[data-start]');
+const stoptBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startBtn.addEventListener('click', getColorBody);
+stoptBtn.addEventListener('click', stopColorBody);
+
+// let currentColorBody = '#ffffff';
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// currentColorBody = getRandomHexColor();
+
+function getColorBody() {
+  startBtn.disabled = true;
+  stoptBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    let currentColorBody = getRandomHexColor();
+    bodyEl.style.backgroundColor = currentColorBody;
+  }, 1000);
+}
+
+function stopColorBody() {
+  startBtn.disabled = false;
+  stoptBtn.disabled = true;
+
+  clearInterval(timerId);
+}
