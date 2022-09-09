e5368e2dc1cab6c26c50149b2848a232fc823d05
OksanaKuzich <komoks255@gmail.com>
2022-09-09 20:10:57 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+  const delayEl = Number(event.currentTarget.delay.value);
+  const stepEl = Number(event.currentTarget.step.value);
+  const amountEl = Number(event.currentTarget.amount.value);
+
+  if (delayEl < 0 || stepEl < 0) {
+    Notify.failure(`Please enter time`);
+    form.reset();
+    return;
+  }
+
+  for (let i = 0; i < amountEl; i++) {
+    const delay = delayEl + stepEl * i;
+    position = i + 1;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.warning(`❌ Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+      });
+  }
+  form.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      // Fulfill
+      resolve({ position, delay });
+    } else {
+      // Reject
+      reject({ position, delay });
+    }
+  });
 }
